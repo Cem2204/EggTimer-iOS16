c967f370806ea046fed43e178a17a7250eca80b8
Cem Yurtdas <cem.cem.strix@gmail.com>
2023-09-20 16:31:50 +0300

    Final Version with Alarm and Progress Bar.

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -1,24 +1,56 @@
 import UIKit
+import AVFoundation
+var player: AVAudioPlayer?
 
 class ViewController: UIViewController {
     
-    let softTime = 5
-    let mediumTime = 8
-    let hardTime = 12
+    let eggTimes = ["Soft" : 300 , "Medium" : 420 , "Hard" : 700 ]
+    
+    @IBOutlet weak var pBar: UIProgressView!
+    @IBOutlet weak var titleLabel: UILabel!
+    var timer = Timer()
+    var totalTime = 0
+    var secondsPassed = 0
     
     @IBAction func click(_ sender: UIButton) {
+        
+        timer.invalidate()
+        totalTime = 0
+        secondsPassed = 0
         let hardness = sender.currentTitle!
-        if hardness == "Soft"{
-            print("Egg is Soft")
-        }
-        else if hardness == "Medium"{
-            print("Egg is Medium")
-        }
-        else if hardness == "Hard"{
-            print("Egg is Hard")
-        }
-
+        titleLabel.text = hardness
+        totalTime = eggTimes[hardness]!
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+        
     }
-    
+    @objc func updateTimer(){
+        if  secondsPassed < totalTime{
+            pBar.progress = Float(secondsPassed) / Float(totalTime)
+            print(pBar.progress)
+            secondsPassed += 1
+        
+        }
+        else{
+            timer.invalidate()
+            pBar.progress = Float(secondsPassed) / Float(totalTime)
+            titleLabel.text = "Done!"
+            playSound()
+        }
+        
+    }
+    func playSound() {
+        guard let path = Bundle.main.path(forResource: "alarm_sound", ofType:"mp3") else {
+            return }
+        let url = URL(fileURLWithPath: path)
+
+        do {
+            player = try AVAudioPlayer(contentsOf: url)
+            player?.play()
+            
+        } catch let error {
+            print(error.localizedDescription)
+        }
+    }
+
     
 }
